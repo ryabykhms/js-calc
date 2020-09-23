3f073e6a5f6dd50d5fa14a171c8326269ad3ff55
Maxim Ryabykh <ryabykhms@gmail.com>
2020-09-23 08:54:19 +0300

    fix: add try/catch for calculator

--- calculator/js/Calculator.js
+++ calculator/js/Calculator.js
@@ -1,13 +1,17 @@
 class Calculator {
   calculate(mathString) {
-    const lexer = new Lexer(mathString);
-    const tokens = lexer.tokenize();
-    const parser = new Parser(tokens);
-    const expressions = parser.parse();
-    let result = "";
-    expressions.forEach((expression) => {
-      result = expression.evaluate();
-    });
-    return result;
+    try {
+      const lexer = new Lexer(mathString);
+      const tokens = lexer.tokenize();
+      const parser = new Parser(tokens);
+      const expressions = parser.parse();
+      let result = "";
+      expressions.forEach((expression) => {
+        result = expression.evaluate();
+      });
+      return result;
+    } catch (error) {
+      return '';
+    }
   }
 }
